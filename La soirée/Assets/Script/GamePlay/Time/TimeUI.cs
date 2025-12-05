46eac8e687ec3f77b478eb593a458ae6225cac64
clement <clement.augoyard@gmail.com>
2025-12-05 16:57:38 +0100

    Ajout temps

---
+++ La soirée/Assets/Script/GamePlay/Time/TimeUI.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace GamePlay
+{
+    public class TimeUI : MonoBehaviour
+    {
+        [SerializeField] private Time timeScript;   // Référence vers le script Time
+        [SerializeField] private Image timeCircle;  // L'image UI du fill radial
+
+        void Update()
+        {
+            if (!timeScript || !timeCircle)
+                return;
+
+            // Convertir le temps en %
+            float fill = (float)timeScript.currentTime / timeScript.totalTime;
+
+            // Appliquer au cercle
+            timeCircle.fillAmount = fill;
+        }
+    }
+}
